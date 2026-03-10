ce30f5213e2cdc46d8eba0dec10ba5413ab69f95
ould-a <Antoine.OuldElhocineTaiariol@cnes.fr>
2026-03-10 16:51:59 +0100

    qgs message bar for the first start

--- __init__.py
+++ __init__.py
@@ -2,20 +2,22 @@ import subprocess
 import os
 import sys
 
+from qgis.core import Qgis
+
 from .xdem_config import (PLUGINDIR,
                           VENVNAME,
                           VENVDIR,
                           PY_XDEM)
 
 def classFactory(iface):
+    setupxdemvenv(iface)
     from .xdem_plugin import XDemPlugin
     return XDemPlugin()
 
 # Virtual environment configuation
-def setupxdemvenv():
+def setupxdemvenv(iface):
     if VENVNAME not in os.listdir(PLUGINDIR):
         subprocess.run([sys.executable, "-m", "venv", VENVDIR])
-        subprocess.Popen([PY_XDEM, "pip", "install", "--upgrade", "pip"])
+        subprocess.Popen([PY_XDEM, "-m", "ensurepip", "--upgrade"])
         subprocess.Popen([PY_XDEM, "-m", "pip", "install", "xdem"])
-
-setupxdemvenv()
+        iface.messageBar().pushMessage("Installing xdem dependencies, please wait a few seconds before using it.", level=Qgis.Info, duration=30)
--- xdem_config.py
+++ xdem_config.py
@@ -3,5 +3,5 @@ import os
 PLUGINDIR = os.path.dirname(__file__)
 VENVNAME = "venvxdem"
 VENVDIR = os.path.join(PLUGINDIR, VENVNAME)
-PY_XDEM = os.path.join(VENVDIR,"bin/python")
+PY_XDEM = os.path.join(VENVDIR, "bin", "python")
 SUBPROCESS_SCRIPT = os.path.join(PLUGINDIR, "subprocess_script.py")
