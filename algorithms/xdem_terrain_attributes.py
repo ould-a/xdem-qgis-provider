cca600de4b974e3cdefcb8e4e3eb65e344c8af4b
ould-a <Antoine.OuldElhocineTaiariol@cnes.fr>
2026-04-24 15:21:02 +0200

    /

--- algorithms/xdem_terrain_attributes.py
+++ algorithms/xdem_terrain_attributes.py
@@ -26,7 +26,8 @@ class TerrainAttributes(XdemProcessingAlgorithm):
         output_path = self.parameterAsOutputLayer(parameters, "OUTPUT", context)
 
         dem = xdem.DEM(dem_path)
-        attribute = self.compute_attribute(dem, parameters, context)
+        function_and_parameters = self.get_attribute_and_parameters(parameters, context)
+        attribute = function_and_parameters(dem)
         attribute.to_file(output_path)
 
         return {"OUTPUT": output_path}
@@ -58,11 +59,10 @@ class Slope(TerrainAttributes):
         parameter.setFlags(parameter.flags() | QgsProcessingParameterDefinition.FlagAdvanced)
         self.addParameter(parameter)
 
-    def compute_attribute(self, dem, parameters, context):
+    def get_attribute_and_parameters(self, parameters, context):
         surface_fit = self.parameterAsString(parameters, "SURFACE_FIT", context)
         degrees = True if self.parameterAsString(parameters, "UNIT", context) == "Degrees" else False
-        slope = dem.slope(surface_fit=surface_fit, degrees=degrees)
-        return slope
+        return lambda dem: dem.slope(surface_fit=surface_fit, degrees=degrees)
     
     def name(self):
         return "Slope"
@@ -94,11 +94,10 @@ class Aspect(TerrainAttributes):
         parameter.setFlags(parameter.flags() | QgsProcessingParameterDefinition.FlagAdvanced)
         self.addParameter(parameter)
 
-    def compute_attribute(self, dem, parameters, context):
+    def get_attribute_and_parameters(self, parameters, context):
         surface_fit = self.parameterAsString(parameters, "SURFACE_FIT", context)
         degrees = True if self.parameterAsString(parameters, "UNIT", context) == "Degrees" else False
-        aspect = dem.aspect(surface_fit=surface_fit, degrees=degrees)
-        return aspect
+        return lambda dem: dem.aspect(surface_fit=surface_fit, degrees=degrees)
     
     def name(self):
         return "Aspect"
@@ -142,16 +141,80 @@ class Hillshade(TerrainAttributes):
         parameter.setFlags(parameter.flags() | QgsProcessingParameterDefinition.FlagAdvanced)
         self.addParameter(parameter)
 
-    def compute_attribute(self, dem, parameters, context):
+    def get_attribute_and_parameters(self, parameters, context):
         surface_fit = self.parameterAsString(parameters, "SURFACE_FIT", context)
         altitude = self.parameterAsInt(parameters, "ALTITUDE", context)
         azimuth = self.parameterAsInt(parameters, "AZIMUTH", context)
         z_factor = self.parameterAsInt(parameters, "ZFACTOR", context)
-        hillshade = dem.hillshade(surface_fit=surface_fit, altitude=altitude, azimuth=azimuth, z_factor=z_factor)
-        return hillshade
+        return lambda dem: dem.hillshade(surface_fit=surface_fit, azimuth=azimuth, altitude=altitude, z_factor=z_factor)
     
     def name(self):
         return "Hillshade"
     
     def createInstance(self):
         return Hillshade()
+
+
+class Curvature(TerrainAttributes):
+    def initAlgorithm(self, config=None):
+        super().initAlgorithm()
+
+        parameter = QgsProcessingParameterEnum(
+            name="SURFACE_FIT",
+            description="Surface fit",
+            options=["Florinsky", "ZevenbergThorne"],
+            defaultValue="Florinsky",
+            usesStaticStrings=True)
+        parameter.setFlags(parameter.flags() | QgsProcessingParameterDefinition.FlagAdvanced)
+        self.addParameter(parameter)
+
+        parameter = QgsProcessingParameterEnum(
+            name="CURV_METHOD",
+            description="Curv method",
+            options=["geometric", "directional"],
+            defaultValue="geometric",
+            usesStaticStrings=True)
+        parameter.setFlags(parameter.flags() | QgsProcessingParameterDefinition.FlagAdvanced)
+        self.addParameter(parameter)
+
+
+class ProfileCurvature(Curvature):
+
+    def get_attribute_and_parameters(self, parameters, context):
+        surface_fit = self.parameterAsString(parameters, "SURFACE_FIT", context)
+        curv_method = self.parameterAsString(parameters, "CURV_METHOD", context)
+        return lambda dem: dem.profile_curvature(surface_fit=surface_fit, curv_method=curv_method)
+    
+    def name(self):
+        return "Profile curvature"
+    
+    def createInstance(self):
+        return ProfileCurvature()
+
+
+class TangentialCurvature(Curvature):
+
+    def get_attribute_and_parameters(self, parameters, context):
+        surface_fit = self.parameterAsString(parameters, "SURFACE_FIT", context)
+        curv_method = self.parameterAsString(parameters, "CURV_METHOD", context)
+        return lambda dem: dem.tangential_curvature(surface_fit=surface_fit, curv_method=curv_method)
+    
+    def name(self):
+        return "Tangential curvature"
+    
+    def createInstance(self):
+        return TangentialCurvature()
+
+
+class PlanformCurvature(Curvature):
+
+    def get_attribute_and_parameters(self, parameters, context):
+        surface_fit = self.parameterAsString(parameters, "SURFACE_FIT", context)
+        curv_method = self.parameterAsString(parameters, "CURV_METHOD", context)
+        return lambda dem: dem.planform_curvature(surface_fit=surface_fit, curv_method=curv_method)
+    
+    def name(self):
+        return "Planform curvature"
+    
+    def createInstance(self):
+        return PlanformCurvature()
