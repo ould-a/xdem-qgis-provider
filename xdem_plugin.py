c8a370ba2108e557cf05bbfd071f034874890f43
ould-a <Antoine.OuldElhocineTaiariol@cnes.fr>
2026-05-12 13:45:52 +0200

    /

--- xdem_plugin.py
+++ xdem_plugin.py
@@ -1,14 +1,5 @@
-import os
-import sys
-import inspect
-
 from qgis.core import QgsApplication
 from .xdem_provider import XdemProvider
-
-cmd_folder = os.path.split(inspect.getfile(inspect.currentframe()))[0]
-
-if cmd_folder not in sys.path:
-    sys.path.insert(0, cmd_folder)
 
 
 class XdemPlugin(object):
