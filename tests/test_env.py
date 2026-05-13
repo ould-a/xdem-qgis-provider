b97d6672cde943881a9f658fe1933694946fe1a9
ould-a <Antoine.OuldElhocineTaiariol@cnes.fr>
2026-05-13 11:09:42 +0200

    /

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,1 +1,19 @@
 import pytest
+import importlib
+
+packages = [
+    "cerberus",
+    "matplotlib",
+    "pytest",
+    "sklearn",
+    "weasyprint",
+    "xdem",
+]
+
+
+def test_packages():
+    for package in packages:
+        try:
+            importlib.import_module(package)
+        except ImportError:
+            pytest.fail(f"{package} is not correctly installed")
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -18,7 +18,7 @@ def test_slope(ref_dem_layer, tmp_path):
     assert output.isValid()
 
 
-def test_coregistration(tba_dem_layer, ref_dem_layer, tmp_path):
+def test_coreg(tba_dem_layer, ref_dem_layer, tmp_path):
     output_path = os.path.join(tmp_path, "test_coreg.tif")
     result = processing.run(
         "XDEM:Coregistration",
@@ -28,6 +28,22 @@ def test_coregistration(tba_dem_layer, ref_dem_layer, tmp_path):
             "MASK": None,
             "METHOD": "Nuth and Kääb (2011)",
             "BLOCKWISE": None,
+            "OUTPUT": output_path,
+        },
+    )
+    output = QgsRasterLayer(result["OUTPUT"])
+    assert output.isValid()
+
+
+def test_bias_corr(tba_dem_layer, ref_dem_layer, tmp_path):
+    output_path = os.path.join(tmp_path, "test_bias_corr.tif")
+    result = processing.run(
+        "XDEM:Bias correction",
+        {
+            "TBA_DEM": tba_dem_layer,
+            "REF_DEM": ref_dem_layer,
+            "MASK": None,
+            "METHOD": "Deramping",
             "OUTPUT": output_path,
         },
     )
